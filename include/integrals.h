ef1b4ec487d459da20fba7f08ea2c5f8fe7099df
SAURON <sauron@gmail.com>
2024-12-05 00:10:40 +0300

    init

--- include/integrals.h
+++
@@ -1,7 +0,0 @@
-#ifndef INTEGRALS_H
-#define INTEGRALS_H
-
-float SinIntegralRect(float A, float B, float e);
-float SinIntegralTrap(float A, float B, float e);
-
-#endif
--- include/pi.h
+++
@@ -1,7 +0,0 @@
-#ifndef PI_H
-#define PI_H
-
-float PiLeibniz(int K);
-float PiWallis(int K);
-
-#endif
